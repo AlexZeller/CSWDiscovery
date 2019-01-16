fd5b7df969b8065680e40cd855739113acaa7b00
Unknown <alexander.zeller@t-online.de>
2019-01-16 15:32:09 +0100

    restructured code; added temporal filter

---
+++ www/functions.js
@@ -0,0 +1,126 @@
+//function to create a Leaflet polygon from bounding box coordinates
+function BBOX2polygon(BBOX) {
+    if (BBOX == null) {
+        return new L.Polygon();
+    }
+    var p1 = new L.LatLng(BBOX[2], BBOX[0]),
+        p2 = new L.LatLng(BBOX[3], BBOX[0]),
+        p3 = new L.LatLng(BBOX[3], BBOX[1]),
+        p4 = new L.LatLng(BBOX[2], BBOX[1]),
+        polygonPoints = [p1, p2, p3, p4];
+
+    return new L.Polygon(polygonPoints);
+}
+
+//function to zoom to the current BBOX layer
+function ZoomToBBOX() {
+    map.fitBounds(BBOX_Layer.getBounds())
+}
+
+//function to get the info of a record of the CSW response 
+function CswRecord(xml) {
+    this.id = $(xml).find('gmd\\:fileIdentifier').children().text();
+    this.title = $(xml).find('gmd\\:identificationInfo').find('gmd\\:citation').find('gmd\\:title').children().text();
+    this.abstract = $(xml).find('gmd\\:identificationInfo').find('gmd\\:abstract').children().text();
+    this.location = $(xml).find('gmd\\:distributionInfo').find('gmd\\:title').children().text();
+    let BBOX = []
+    this.westBoundLongitude = $(xml).find('gmd\\:identificationInfo').find('gmd\\:EX_GeographicBoundingBox').find('gmd\\:westBoundLongitude').children().text();
+    this.eastBoundLongitude = $(xml).find('gmd\\:identificationInfo').find('gmd\\:EX_GeographicBoundingBox').find('gmd\\:eastBoundLongitude').children().text();
+    this.southBoundLatitude = $(xml).find('gmd\\:identificationInfo').find('gmd\\:EX_GeographicBoundingBox').find('gmd\\:southBoundLatitude').children().text();
+    this.northBoundLatitude = $(xml).find('gmd\\:identificationInfo').find('gmd\\:EX_GeographicBoundingBox').find('gmd\\:northBoundLatitude').children().text();
+    BBOX.push(this.westBoundLongitude)
+    BBOX.push(this.eastBoundLongitude)
+    BBOX.push(this.southBoundLatitude)
+    BBOX.push(this.northBoundLatitude)
+    this.BBOX = BBOX
+
+    records.push({
+        id: this.id,
+        BBOX: this.BBOX
+    });
+}
+
+//function to style the information of the record to append to the html table
+function styleEntry(rec) {
+    let snippet
+    snippet += "<tr><td data-id='" + rec.id + "'>"
+    link = 'http://136.199.176.14:8080/geonetwork/srv/ger/catalog.search#/metadata/' + rec.id
+    snippet += "<strong>" + rec.title + "</strong>"
+    snippet += "<div id='Zoom'><img src='img/search.png' onclick='ZoomToBBOX()'></div></br>"
+    snippet += "<small><i><a href='" + link + "' target='_blank'>" + rec.id + "</a></i></small></br>"
+    snippet += "<a><small>" + rec.abstract + "</small></a></br>"
+    snippet += "<a><small>" + rec.location + "</small></a>"
+    snippet += "</td></tr>"
+
+    return snippet
+}
+
+//function to construct the POST request
+function buildCSWRequest(startPosition, bbox_enabled, freetext, startDate, endDate) {
+
+    //Start of the request
+    csw_request = '<csw:GetRecords maxRecords="' + number_of_results + '" startPosition="' + startPosition + '" outputFormat="application/xml" outputSchema="http://www.isotc211.org/2005/gmd"'
+                  + ' resultType="results" service="CSW" version="2.0.2" xsi:schemaLocation="http://www.opengis.net/cat/csw/2.0.2 http://schemas.opengis.net/csw/2.0.2/CSW-discovery.xsd"'
+                  + ' xmlns:ogc="http://www.opengis.net/ogc" xmlns:csw="http://www.opengis.net/cat/csw/2.0.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">'
+                  + '<csw:Query typeNames="csw:Record"><csw:ElementSetName>full</csw:ElementSetName>';
+
+    // construct snippet for BBOX
+    if (bbox_enabled == true) {
+        bounds = map.getBounds();
+        spatialFilter = '<ogc:BBOX><ogc:PropertyName>ows:BoundingBox</ogc:PropertyName><gml:Envelope xmlns:gml="http://www.opengis.net/gml"><gml:lowerCorner>' + bounds.getWest() +
+            ' ' + bounds.getSouth() + '</gml:lowerCorner><gml:upperCorner>' + bounds.getEast() + ' ' + bounds.getNorth() + '</gml:upperCorner></gml:Envelope></ogc:BBOX>';
+    } 
+
+    // construct snippet for Date range
+    if (startDate != null && endDate != null) {
+        temporalFilter = '<ogc:PropertyIsGreaterThanOrEqualTo><ogc:PropertyName>csw:TempExtent_begin</ogc:PropertyName><ogc:Literal>'
+                         + startDate + '</ogc:Literal></ogc:PropertyIsGreaterThanOrEqualTo><ogc:PropertyIsLessThanOrEqualTo><ogc:PropertyName>csw:TempExtent_end</ogc:PropertyName><ogc:Literal>' 
+                         + endDate + '</ogc:Literal></ogc:PropertyIsLessThanOrEqualTo>';
+    }       
+
+    //construct snippet for freetext
+    if (freetext != '') {
+        freetextFilter = '<ogc:PropertyIsLike escapeChar="\\" singleChar="_" wildCard="%"><ogc:PropertyName>csw:AnyText</ogc:PropertyName>' +
+            '<ogc:Literal>%' + freetext + '%</ogc:Literal></ogc:PropertyIsLike>'
+    }
+
+    if (freetext != '') {
+
+        if (bbox_enabled == true) {
+
+            if (startDate != null && endDate != null) {
+                csw_request += '<csw:Constraint version="1.1.0"><ogc:Filter><ogc:And>' + freetextFilter + spatialFilter + temporalFilter + '</ogc:And></ogc:Filter></csw:Constraint>';    
+            } else {
+                csw_request += '<csw:Constraint version="1.1.0"><ogc:Filter><ogc:And>' + freetextFilter + spatialFilter + '</ogc:And></ogc:Filter></csw:Constraint>';
+            }
+        } else if (startDate != null && endDate != null) {
+
+            csw_request += '<csw:Constraint version="1.1.0"><ogc:Filter><ogc:And>' + freetextFilter + temporalFilter + '</ogc:And></ogc:Filter></csw:Constraint>';    
+
+        } else {
+
+            csw_request += '<csw:Constraint version="1.1.0"><ogc:Filter>'+ freetextFilter +'</ogc:Filter></csw:Constraint>';
+    
+        }
+
+    } else if (bbox_enabled == true) {
+
+        if (startDate != null && endDate != null) {
+            csw_request += '<csw:Constraint version="1.1.0"><ogc:Filter><ogc:And>' + spatialFilter + temporalFilter + '</ogc:And></ogc:Filter></csw:Constraint>';
+        } else {
+            csw_request += '<csw:Constraint version="1.1.0"><ogc:Filter>' + spatialFilter + '</ogc:Filter></csw:Constraint>';
+        }
+
+    } else if (startDate != null && endDate != null) {
+
+        csw_request += '<csw:Constraint version="1.1.0"><ogc:Filter><ogc:And>' + temporalFilter + '</ogc:And></ogc:Filter></csw:Constraint>';
+
+    } 
+
+    //End of the request
+    csw_request += '</csw:Query></csw:GetRecords>';
+
+    return csw_request;
+
+}
+
--- www/main.js
+++ www/main.js
@@ -1,3 +1,6 @@
+//url of CSW
+const csw_url = 'http://192.168.178.27:8080/geonetwork/srv/en/csw'
+
 //define Leaflet map, set view on lat/lon coordinates of Trier
 var map = L.map('map').setView([49.76, 6.648387], 5);
 //use openstreetmap as basemap
@@ -7,73 +10,28 @@ basemap = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 //add basemap to map
 map.addLayer(basemap);
 
-//define BBOX layer
-let BBOX_Layer
-//list of current records
+
+//list of current records; used to find the matching BBOX when hovered over entry
 let records = []
 
-//function to create Leaflet polygon from bounding box
-function BBOX2polygon(BBOX) {
-    if (BBOX == null) {
-        return new L.Polygon();
-    }
-    var p1 = new L.LatLng(BBOX[2], BBOX[0]),
-        p2 = new L.LatLng(BBOX[3], BBOX[0]),
-        p3 = new L.LatLng(BBOX[3], BBOX[1]),
-        p4 = new L.LatLng(BBOX[2], BBOX[1]),
-        polygonPoints = [p1, p2, p3, p4];
-
-    return new L.Polygon(polygonPoints);
-}
-
-//function to get the info of the record of the CSW response 
-function CswRecord(xml) {
-    this.id = $(xml).find('gmd\\:fileIdentifier').children().text();
-    this.title = $(xml).find('gmd\\:identificationInfo').find('gmd\\:citation').find('gmd\\:title').children().text();
-    this.abstract = $(xml).find('gmd\\:identificationInfo').find('gmd\\:abstract').children().text();
-    this.location = $(xml).find('gmd\\:distributionInfo').find('gmd\\:title').children().text(); 
-    let BBOX = []
-    this.westBoundLongitude = $(xml).find('gmd\\:identificationInfo').find('gmd\\:EX_GeographicBoundingBox').find('gmd\\:westBoundLongitude').children().text();
-    this.eastBoundLongitude = $(xml).find('gmd\\:identificationInfo').find('gmd\\:EX_GeographicBoundingBox').find('gmd\\:eastBoundLongitude').children().text();
-    this.southBoundLatitude = $(xml).find('gmd\\:identificationInfo').find('gmd\\:EX_GeographicBoundingBox').find('gmd\\:southBoundLatitude').children().text();
-    this.northBoundLatitude = $(xml).find('gmd\\:identificationInfo').find('gmd\\:EX_GeographicBoundingBox').find('gmd\\:northBoundLatitude').children().text();
-    BBOX.push(this.westBoundLongitude)
-    BBOX.push(this.eastBoundLongitude)
-    BBOX.push(this.southBoundLatitude)
-    BBOX.push(this.northBoundLatitude)
-    this.BBOX = BBOX
-
-    records.push({id: this.id, BBOX: this.BBOX});
-}
-
-//function to style the information of the record to append to the html
-function styleEntry(rec){
-   let snippet
-   snippet +=  "<tr><td data-id='" + rec.id + "'>"
-   link = 'http://136.199.176.14:8080/geonetwork/srv/ger/catalog.search#/metadata/' + rec.id
-   snippet += "<strong>" + rec.title + "</strong>"
-   snippet += "<div id='Zoom'><img src='img/search.png' onclick='ZoomToBBOX()'></div></br>"
-   snippet += "<small><i><a href='" + link + "' target='_blank'>" + rec.id + "</a></i></small></br>"
-   snippet += "<a><small>" + rec.abstract + "</small></a></br>"
-   snippet += "<a><small>" + rec.location + "</small></a>"
-   snippet +="</td></tr>"
-
-   return snippet
-}
-
-//url of CSW
-const csw_url = 'http://136.199.176.14:8080/geonetwork/srv/en/csw'
+//define BBOX layer
+let BBOX_Layer
 
 //Parameters for CSW GetRecords
+//number of records returned
 const number_of_results = 10
-
-let matched; 
+//Values to get to previous/next records 
+let matched;
 let nextrecord;
 let start;
 
-//function to send the POST request to the CSW 
-function getRecords(startPosition){
-    
+//Temportal filter values
+let startDate
+let endDate
+
+//function to send the POST request to the CSW; parameter startPosition to traverse through the records
+function getRecords(startPosition) {
+
     //empty list to fill with current records
     records = []
 
@@ -87,30 +45,7 @@ function getRecords(startPosition){
     var freetext = $('#input-anytext').val().trim();
     var bbox_enabled = $('#input-spatialFilter').is(':checked');
 
-    csw_request = '<csw:GetRecords maxRecords="' + number_of_results + '" startPosition="' + startPosition + '" outputFormat="application/xml" outputSchema="http://www.isotc211.org/2005/gmd" resultType="results" service="CSW" version="2.0.2" xsi:schemaLocation="http://www.opengis.net/cat/csw/2.0.2 http://schemas.opengis.net/csw/2.0.2/CSW-discovery.xsd" xmlns:ogc="http://www.opengis.net/ogc" xmlns:csw="http://www.opengis.net/cat/csw/2.0.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><csw:Query typeNames="csw:Record"><csw:ElementSetName>full</csw:ElementSetName>';
-
-    if (bbox_enabled != false) {
-        bounds = map.getBounds();
-        //qbbox = '<ogc:BBOX><ogc:PropertyName>ows:BoundingBox</ogc:PropertyName><gml:Envelope xmlns:gml="http://www.opengis.net/gml"><gml:lowerCorner>' + bounds.getSouth() + ' ' + bounds.getWest() + '</gml:lowerCorner><gml:upperCorner>' + bounds.getNorth() + ' ' + bounds.getEast() + '</gml:upperCorner></gml:Envelope></ogc:BBOX>';
-        qbbox = '<ogc:BBOX><ogc:PropertyName>ows:BoundingBox</ogc:PropertyName><gml:Envelope xmlns:gml="http://www.opengis.net/gml"><gml:lowerCorner>' + bounds.getWest() + ' ' + bounds.getSouth() + '</gml:lowerCorner><gml:upperCorner>' + bounds.getEast() + ' ' + bounds.getNorth() + '</gml:upperCorner></gml:Envelope></ogc:BBOX>';
-    }
-
-    if (freetext != '') {
-        csw_request += '<csw:Constraint version="1.1.0"><ogc:Filter><ogc:PropertyIsLike escapeChar="\\" singleChar="_" wildCard="%"><ogc:PropertyName>csw:AnyText</ogc:PropertyName><ogc:Literal>%' + freetext + '%</ogc:Literal></ogc:PropertyIsLike></ogc:Filter></csw:Constraint>';
-
-
-        if (bbox_enabled != false) {
-            csw_request += '<csw:Constraint version="1.1.0"><ogc:Filter><ogc:And><ogc:PropertyIsLike escapeChar="\\" singleChar="_" wildCard="%"><ogc:PropertyName>csw:AnyText</ogc:PropertyName><ogc:Literal>%' + freetext + '%</ogc:Literal></ogc:PropertyIsLike>' + qbbox + '</ogc:And></ogc:Filter></csw:Constraint>';
-        }
-        else {
-            csw_request += '<csw:Constraint version="1.1.0"><ogc:Filter><ogc:PropertyIsLike escapeChar="\\" singleChar="_" wildCard="%"><ogc:PropertyName>csw:AnyText</ogc:PropertyName><ogc:Literal>%' + freetext + '%</ogc:Literal></ogc:PropertyIsLike></ogc:Filter></csw:Constraint>';
-        }
-
-    } else if (bbox_enabled != false) {
-        csw_request += '<csw:Constraint version="1.1.0"><ogc:Filter>' + qbbox + '</ogc:Filter></csw:Constraint>';
-    }
-
-    csw_request += '</csw:Query></csw:GetRecords>';
+    csw_request = buildCSWRequest(startPosition, bbox_enabled, freetext, startDate, endDate)
 
     $.ajax({
         type: "post",
@@ -118,7 +53,7 @@ function getRecords(startPosition){
         contentType: "text/xml",
         data: csw_request,
         dataType: "xml",
-        success: function(xml) {
+        success: function (xml) {
 
             $('#table-csw-results').empty();
             //console.log(xml);
@@ -131,25 +66,23 @@ function getRecords(startPosition){
                 $('#recordsNumber').html('');
                 return;
             }
-            
-            if (nextrecord == 0 || nextrecord >= matched) { 
+
+            if (nextrecord == 0 || nextrecord >= matched) {
                 $('#li-next').addClass('disabled');
                 nextrecord = matched;
-            }
-            else {
+            } else {
                 $('#li-next').removeClass('disabled');
             }
 
             if (startPosition == 1) {
                 $('#li-previous').addClass('disabled');
-            }
-            else {
+            } else {
                 $('#li-previous').removeClass('disabled');;
             }
 
             $("#recordsNumber").text(startPosition + " - " + nextrecord + " of " + matched + " Record(s)");
 
-            $(xml).find('gmd\\:MD_Metadata').each(function(record) {
+            $(xml).find('gmd\\:MD_Metadata').each(function (record) {
 
                 let rec = new CswRecord($(this));
                 $("#table-csw-results").append(styleEntry(rec));
@@ -161,11 +94,11 @@ function getRecords(startPosition){
 
 
 
-$(document).ready(function(){
+$(document).ready(function () {
 
     getRecords();
 
-    $("table").on("mouseenter", "td", function(event) {
+    $("table").on("mouseenter", "td", function (event) {
         let id = $(this).data("id")
         //console.log(id)
         let entry = records.find(e => e.id === id);
@@ -178,21 +111,21 @@ $(document).ready(function(){
         map.addLayer(BBOX_Layer)
     });
 
-    $('#a-previous').click(function(event){
-        event.preventDefault(); 
-        startposition2 = start-number_of_results;
+    $('#a-previous').click(function (event) {
+        event.preventDefault();
+        startposition2 = start - number_of_results;
         if (startposition2 < 1) {
             return;
         }
         getRecords(startposition2);
-    }); 
-    $('#a-next').click(function(event){
-        event.preventDefault(); 
-        if (nextrecord == 0 || nextrecord>=matched) {
+    });
+    $('#a-next').click(function (event) {
+        event.preventDefault();
+        if (nextrecord == 0 || nextrecord >= matched) {
             return;
         }
         getRecords(nextrecord);
-    }); 
+    });
 
     $("#input-anytext").keypress(function (e) {
         if (e.keyCode == 13) { // Enter key pressed, but not submitting the form to a page refresh
@@ -240,20 +173,25 @@ $(document).ready(function(){
         },
         "minDate": "1/1/1950",
         "maxDate": "1/1/2050"
-    }, function(start, end, label) {
-      console.log('New date range selected: ' + start.format('DD.MM.YYYY') + ' to ' + end.format('DD.MM.YYYY'));
     });
 
-    $('input[name="daterange"]').on('apply.daterangepicker', function(ev, picker) {
+    $('input[name="daterange"]').on('apply.daterangepicker', function (ev, picker) {
         $(this).val(picker.startDate.format('DD.MM.YYYY') + ' - ' + picker.endDate.format('DD.MM.YYYY'));
+        startDate = picker.startDate.format('YYYY-MM-DD')
+        endDate = picker.endDate.format('YYYY-MM-DD')
+        console.log(startDate, endDate)
+
     });
 
-    $('input[name="daterange"]').on('cancel.daterangepicker', function(ev, picker) {
+    $('input[name="daterange"]').on('cancel.daterangepicker', function (ev, picker) {
         $(this).val('');
+        startDate = null
+        endDate = null
+        console.log(startDate, endDate)
     });
 
-    
-    map.on('move', function() {
+
+    map.on('move', function () {
         var bbox_enabled = $('#input-spatialFilter').is(':checked');
         if (bbox_enabled != false) {
             getRecords();
@@ -261,15 +199,4 @@ $(document).ready(function(){
         }
     });
 
-
-
-    
-    
-
 });
-
-function ZoomToBBOX(){
-
-    map.fitBounds(BBOX_Layer.getBounds())
-
-}
